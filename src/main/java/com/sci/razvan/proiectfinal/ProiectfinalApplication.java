e94dd018692a622bf1d038508ed327fa944a4d45
Razvan Bineata <simona>
2019-04-24 19:41:05 +0300

    first commit, making setup for project

---
+++ src/main/java/com/sci/razvan/proiectfinal/ProiectfinalApplication.java
@@ -0,0 +1,13 @@
+package com.sci.razvan.proiectfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectfinalApplication.class, args);
+	}
+
+}
